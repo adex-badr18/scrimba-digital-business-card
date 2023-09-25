8baa06f54e8c1c707c47d2004723df82dcbad2a3
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-09-25 16:53:31 +0100

    Create Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+    return (
+        <footer className="footer">
+            <div className="footer--social-links">
+                <i class="fa-brands fa-square-x-twitter"></i>
+                <i class="fa-brands fa-square-facebook"></i>
+                <i class="fa-brands fa-square-instagram"></i>
+                <i class="fa-brands fa-square-github"></i>
+            </div>
+        </footer>
+    )
+}

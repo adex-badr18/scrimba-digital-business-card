ff14ab669520466069ddd6a331697088f141bfef
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-09-25 17:18:17 +0100

    Render main & footer components on page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
-import reactLogo from './assets/react.svg'
+import reactLogo from './assets/react.svg';
 import Info from './components/Info';
+import About from './components/About';
+import Interests from './components/Interests';
+import Footer from './components/Footer';
 import './App.css'
 
 function App() {
@@ -7,6 +10,13 @@ function App() {
   return (
     <div className='container'>
       <Info />
+      
+      <main>
+        <About />
+        <Interests />
+      </main>
+
+      <Footer />
     </div>
   )
 }

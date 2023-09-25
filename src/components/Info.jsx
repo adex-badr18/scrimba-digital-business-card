c0b85bde35285ff30f7b219c458345a6cfadb7fe
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-09-25 16:12:05 +0100

    Create Info component markup

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,7 +1,25 @@
+import dp from '../assets/images/dp.jpg';
+
 export default function Info() {
     return (
         <header>
-            <img src="{}" alt="Display picture of Badrudeen" />
+            <img src={dp} className='info--img' alt="Display picture of Badrudeen" />
+
+            <div className="info--bio">
+                <h1 className="info--name">Badrudeen A. Abdul-hameed</h1>
+                <p className="info--role">Frontend Developer</p>
+                <a href="#" className="info--website">badrudeen.website</a>
+            </div>
+
+            <div className="info--cta-container">
+                <button className="info--cta-email">
+                <i class="fa-solid fa-envelope"></i> Email
+                </button>
+
+                <button className="info--cta-email">
+                <i class="fa-brands fa-linkedin"></i> LinkedIn
+                </button>
+            </div>
         </header>
     )
 }

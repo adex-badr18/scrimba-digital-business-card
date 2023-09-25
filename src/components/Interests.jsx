fa3a2fde945c2c01affdfd6aed7f532476c71f0a
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-09-25 16:53:07 +0100

    Create Interests component

---
+++ src/components/Interests.jsx
@@ -0,0 +1,8 @@
+export default function Interests() {
+    return (
+        <section className="interests">
+            <h3 className="interests--title">Interests</h3>
+            <p className="interests--description">Food expert. Reader. Internet fanatic. Code master. Culture explorer. Travel geek. Coffee fanatic.</p>
+        </section>
+    )
+}

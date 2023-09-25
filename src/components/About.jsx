9f6d54d6f24ee69f649dcb17826c7a1e8a513d61
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-09-25 16:24:07 +0100

    Create About component

---
+++ src/components/About.jsx
@@ -0,0 +1,8 @@
+export default function About() {
+    return (
+        <section className="about">
+            <h3 className="about--title">About</h3>
+            <p className="about--description">Aspiring Junior Frontend Developer with a strong foundation in React.js. Committed to crafting user-centric interfaces with a focus on continuous learning and growth. Enthusiastic about contributing to collaborative projects and staying current in the ever-evolving world of web development.</p>
+        </section>
+    )
+}

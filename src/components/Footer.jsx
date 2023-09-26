6167d758be68e2c72d0e942245f0d4b9ad38a0db
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-09-26 19:31:57 +0100

    Wrap footer icons in anchor tags

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,10 +2,11 @@ export default function Footer() {
     return (
         <footer className="footer--container">
             <div className="footer--social-links">
-                <i className="fa-brands fa-square-x-twitter"></i>
-                <i className="fa-brands fa-square-facebook"></i>
-                <i className="fa-brands fa-square-instagram"></i>
-                <i className="fa-brands fa-square-github"></i>
+                <a href=""><i className="fa-brands fa-square-x-twitter"></i></a>
+                <a href=""><i className="fa-brands fa-square-facebook"></i></a>
+                <a href=""><i className="fa-brands fa-square-instagram"></i></a>
+                <a href=""><i className="fa-brands fa-square-github"></i></a>
+                
             </div>
         </footer>
     )

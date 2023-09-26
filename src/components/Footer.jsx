7338b84dfcdade831e668084e9405606ee39f504
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-09-26 20:57:57 +0100

    Add url to footer icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,10 +2,10 @@ export default function Footer() {
     return (
         <footer className="footer--container">
             <div className="footer--social-links">
-                <a href=""><i className="fa-brands fa-square-x-twitter"></i></a>
-                <a href=""><i className="fa-brands fa-square-facebook"></i></a>
-                <a href=""><i className="fa-brands fa-square-instagram"></i></a>
-                <a href=""><i className="fa-brands fa-square-github"></i></a>
+                <a href="https://twitter.com/Fortunate1790"><i className="fa-brands fa-square-x-twitter"></i></a>
+                <a href="https://web.facebook.com/badrudeenadewumi"><i className="fa-brands fa-square-facebook"></i></a>
+                <a href="https://www.instagram.com/"><i className="fa-brands fa-square-instagram"></i></a>
+                <a href="https://github.com/adex-badr18"><i className="fa-brands fa-square-github"></i></a>
                 
             </div>
         </footer>

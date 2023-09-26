8f6cfcdaff92f68867a6b59b029e06a4dcea2677
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-09-26 10:51:25 +0100

    Style the footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className="footer">
+        <footer className="footer--container">
             <div className="footer--social-links">
                 <i class="fa-brands fa-square-x-twitter"></i>
                 <i class="fa-brands fa-square-facebook"></i>
